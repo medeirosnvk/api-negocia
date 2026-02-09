810f605280f8cf309aaeac56c3bd7d05cd608cc8
Kevin Medeiros <108627831+medeirosnvk@users.noreply.github.com>
2026-02-09 14:10:54 -0300

    wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ export function useTheme() {
 }
 
 function App() {
-  const [theme, setTheme] = useState<'dark' | 'light'>('dark');
+  const [theme, setTheme] = useState<'dark' | 'light'>('light');
 
   useEffect(() => {
     document.documentElement.classList.toggle('light', theme === 'light');
